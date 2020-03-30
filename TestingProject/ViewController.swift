6d7258c195236e695d97d2a0606b3c0009ebfbb5
Cuong Manh Nguyen <cuongmanhnguyen@kms-technology.com>
2020-03-30 11:53:37 +0700

    Initial Commit

---
+++ TestingProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingProject
+//
+//  Created by Cuong Manh Nguyen on 3/30/20.
+//  Copyright © 2020 KMS Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
